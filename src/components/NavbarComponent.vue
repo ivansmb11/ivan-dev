df92ec87edebd6fb26359ccde4d13296e6000f6c
ivansmb11 <22760520+ivansmb11@users.noreply.github.com>
2022-07-23 01:01:07 -0500

    first commit

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,12 @@
+<template>
+<nav class="navbar navbar-dark bg-dark">
+  <router-link :to="{name: 'home'}" class="no-style">Home</router-link>
+</nav>
+</template>
+
+<style>
+.no-style {
+  text-decoration: none;
+  color: #ffffff;
+}
+</style>
