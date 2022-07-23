871916b14d44e153b308d5fdf3c5ad69c2ed1180
ivansmb11 <ismb9@hotmail.com>
2022-07-23 01:01:07 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import NavbarComponent from '@/components/NavbarComponent.vue'
+
+
+</script>
+
+<template>
+  <NavbarComponent />
+</template>
+
+<style scoped>
+
+</style>
