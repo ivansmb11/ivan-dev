44483548f27a3f906d9bc3684aefbbc3e1f68796
ivansmb11 <22760520+ivansmb11@users.noreply.github.com>
2022-07-25 17:43:42 -0500

    new path functions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <template>
-  Home
+<div class="container mw-100 vh-100">
+  <span>Home</span>
+</div>
 </template>
 
 <script>
 
 </script>
 
-<style>
-
-</style>
+<!-- <style scoped>
+.container {
+  background: #000000;
+  background-image: url("../assets/home.jpg");
+}
+</style> -->
